5e92d985195a7e3f1ec86e4284863320a401a77b
Sz4by <szaby.korodi@yahoo.com>
2024-09-08 15:15:35 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
-/*  
-    Attention recruiters or anyone that might be viewing this,
-    obviously I'd gitignore this file, but this app is for exhibition purposes
-*/
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// https://console.firebase.google.com/u/0/project/chat-rooms-daf8d/database/firestore/data~2F
-const config = {
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyBiIir5Y5r0bsbyducte7PpVreWXzu-o-M",
   authDomain: "szaby-5d8be.firebaseapp.com",
   databaseURL: "https://szaby-5d8be-default-rtdb.firebaseio.com",
@@ -13,11 +15,11 @@ const config = {
   messagingSenderId: "215276216449",
   appId: "1:215276216449:web:94f0f17cbd74c2336fad80",
   measurementId: "G-EL7YSFVQZ0"
-}
+};
 
 // Initialize Firebase
-firebase.initializeApp(config)
-const db = firebase.firestore()
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Password for the private chat room
 const privateRoomPassword = 'DamnTrainCJ!69'
